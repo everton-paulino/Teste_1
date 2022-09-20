cb1b9f052770d9b80074015c5fee36aef6ca0de3
Evertonpaulino1984 <evertonpaulino1984@hotmail.com>
2022-09-20 19:28:34 -0300

    Adicione README.md e LICENSE.txt.

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Teste1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int Indice = 13;
+            int Soma = 0;
+            int K = 0;
+            
+
+
+
+            while (K < Indice)
+            {
+                     
+                Soma = Soma + K;
+                K = K + 1;
+                Console.WriteLine(Soma);
+
+
+            }
+
+            Console.ReadLine();
+
+        }
+    }
+}
